054ffa718152ce14c2de41f365f45a82b46efbcb
Gabriele Fenanda <gabifernanda@gmail.com>
2016-10-20 11:33:14 -0200

    TA FUNCIONANDO ABRIR A CAMERA PORRAAAA

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('onesv', ['ionic'])
+angular.module('starter', ['ionic', 'ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -23,8 +23,7 @@ angular.module('onesv', ['ionic'])
   });
 })
 
-module.controller('videoController', function($scope, $cordovaCapture){
-  $scope.videoURL = "https://www.youtube.com/watch?v=wJCUNMMvVnE";
+.controller('videoController', function($scope, $cordovaCapture){
 
   $scope.captureVideo = function(){
     var options = {limit: 1, duration: 1};
